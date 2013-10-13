657bd87fc09ed98b0926d63dfcc0466df8e7510f
Leonardo Menezes <leonardo.menezes@xing.com>
2013-10-13 12:03:02 +0200

    remember dist dir!

--- dist/kopf.js
+++ dist/kopf.js
@@ -1253,10 +1253,7 @@ function ElasticClient(host,username,password) {
 						xhr.setRequestHeader("Authorization", auth);
 					} 
 				},
-				data: data,
-				xhrFields: {
-					withCredentials: true
-				}
+				data: data
 		})).then(
 			function(r) { 
 				callback_success(r); 
