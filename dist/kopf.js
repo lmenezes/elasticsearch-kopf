e97843854c4a32181c243dda8cedb1a941d2a348
Leonardo Menezes <mail@lmenezes.com>
2014-01-09 00:13:00 +0100

    limit the number of alerts to 3
    closes #36

--- dist/kopf.js
+++ dist/kopf.js
@@ -2053,6 +2053,8 @@ var Alert=function(message, response, level, _class, icon) {
 }
 
 kopf.factory('AlertService', function() {
+	this.max_alerts = 3;
+
 	this.alerts = [];
 	
 	// removes ALL alerts
@@ -2070,26 +2072,26 @@ kopf.factory('AlertService', function() {
 	
 	// creates an error alert
 	this.error=function(message, response) {
-		var alert = new Alert(message, response, "error", "alert-danger", "icon-warning-sign");
-		this.alerts.unshift(alert);
-		var service = this;
-		setTimeout(function() { service.remove(alert.id) }, 30000);
+		this.addAlert(new Alert(message, response, "error", "alert-danger", "icon-warning-sign"), 15000);
 	}
 	
 	// creates an info alert
 	this.info=function(message, response) {
-		var alert = new Alert(message, response, "info", "alert-info", "icon-info");
-		this.alerts.unshift(alert);
-		var service = this;
-		setTimeout(function() { service.remove(alert.id) }, 5000);		
+		this.addAlert(new Alert(message, response, "info", "alert-info", "icon-info"), 5000);
 	}
 	
 	// creates success alert
 	this.success=function(message, response) {
-		var alert = new Alert(message, response, "success", "alert-success", "icon-ok");
+		this.addAlert(new Alert(message, response, "success", "alert-success", "icon-ok"), 5000);
+	}
+	
+	this.addAlert=function(alert, timeout) {
 		this.alerts.unshift(alert);
 		var service = this;
-		setTimeout(function() { service.remove(alert.id) }, 5000);
+		setTimeout(function() { service.remove(alert.id) }, timeout);		
+		if (this.alerts.length >= this.max_alerts) {
+			this.alerts.length = 3;
+		}
 	}
 	
 	return this;
