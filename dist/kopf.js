9a48ebcd9d6f7a377715b57f9ebf8efd39ec8b96
Leonardo Menezes <mail@lmenezes.com>
2014-02-01 14:00:23 +0100

    improved navigation bar for fluid layout
    still missing fixing the collapse point

--- dist/kopf.js
+++ dist/kopf.js
@@ -1927,10 +1927,12 @@ function NavbarController($scope, $location, $timeout, AlertService, SettingsSer
 	$scope.settings_service = SettingsService;
 	$scope.new_refresh = $scope.settings_service.getRefreshInterval();
 	
-    $scope.connectToHost=function() {
-		if (isDefined($scope.new_host) && $scope.new_host.length > 0) {
-			$scope.setHost($scope.new_host);
-			$scope.refreshClusterState();
+    $scope.connectToHost=function(event) {
+		if (event.keyCode == 13) {
+			if (isDefined($scope.new_host) && $scope.new_host.length > 0) {
+				$scope.setHost($scope.new_host);
+				$scope.refreshClusterState();
+			}			
 		}
 	};
 	
