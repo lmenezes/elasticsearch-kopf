f5a447867fc1458e0ed19ccec307ddf75babcb6c
Leonardo Menezes <leonardo.menezes@xing.com>
2014-04-21 11:59:33 +0200

    version bump 0.5.7

--- src/kopf/controllers/global.js
+++ src/kopf/controllers/global.js
@@ -1,6 +1,6 @@
 function GlobalController($scope, $location, $timeout, $sce, ConfirmDialogService, AlertService, SettingsService, ThemeService) {
 	$scope.dialog = ConfirmDialogService;
-	$scope.version = "0.5.6";
+	$scope.version = "0.5.7";
 	$scope.username = null;
 	$scope.password = null;
 	$scope.alert_service = AlertService;
