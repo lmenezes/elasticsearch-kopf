cdf3248b24127618ec67c1965bb01b1d9cf0cef4
Leonardo Menezes <leonardo.menezes@xing.com>
2014-08-05 21:08:17 +0200

    extended tests for cluster overview

--- tests/jasmine/overview.tests.js
+++ tests/jasmine/overview.tests.js
@@ -14,7 +14,7 @@ describe('ClusterOverviewController', function(){
         this.createController = function() {
             return $controller('ClusterOverviewController', {$scope: this.scope}, this.IndexSettingsService, this.ConfirmDialogService, this.AlertService);
         };
-
+        this.scope.updateModel=function(body) { body(); };
         this._controller = this.createController();
     }));
 
@@ -43,7 +43,6 @@ describe('ClusterOverviewController', function(){
         expect(this.scope.nodes).toEqual([]);
     });
 
-    //TESTS
     it('should detect when cluster changes and update indices and nodes', function(){
         // paginator
         this.scope.cluster = { indices: [ 1, 2, 3 ], nodes: [ 3, 2, 1] };
@@ -154,5 +153,231 @@ describe('ClusterOverviewController', function(){
     });
 
 
+    // shutdown node
+    it('shutdownNode must invoke client method, display alert and refresh model', function(){
+        this.scope.client.shutdownNode=function(node_id, success, failed) { return success(); };
+        this.scope.refreshClusterState=function(){};
+        spyOn(this.scope, "refreshClusterState").andReturn(true);
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "shutdownNode").andCallThrough();
+        spyOn(this.AlertService, "success").andReturn(true);
+        this.scope.shutdownNode("node_id");
+        expect(this.scope.client.shutdownNode).toHaveBeenCalledWith("node_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.refreshClusterState).toHaveBeenCalled();
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.success).toHaveBeenCalled();
+    });
+
+    it('shutdownNode must display error message if operation fails', function(){
+        this.scope.client.shutdownNode=function(node_id, success, failed) { return failed(); };
+        spyOn(this.scope.client, "shutdownNode").andCallThrough();
+        spyOn(this.AlertService, "error").andReturn(true);
+        spyOn(this.scope, "updateModel").andCallThrough();
+        this.scope.shutdownNode("node_id");
+        expect(this.scope.client.shutdownNode).toHaveBeenCalledWith("node_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.error).toHaveBeenCalled();
+    });
+
+    // optimizeIndex
+    it('optimizeIndex must invoke client method, display alert and refresh model', function(){
+        this.scope.client.optimizeIndex=function(index, success, failed) { return success(); };
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "optimizeIndex").andCallThrough();
+        spyOn(this.AlertService, "success").andReturn(true);
+        this.scope.optimizeIndex("index_id");
+        expect(this.scope.client.optimizeIndex).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.success).toHaveBeenCalled();
+    });
+
+    it('optimizeIndex must display error message if operation fails', function(){
+        this.scope.client.optimizeIndex=function(index, success, failed) { return failed(); };
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "optimizeIndex").andCallThrough();
+        spyOn(this.AlertService, "error").andReturn(true);
+        this.scope.optimizeIndex("index_id");
+        expect(this.scope.client.optimizeIndex).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.error).toHaveBeenCalled();
+    });
+
+    // delete index
+    it('deleteIndex must invoke client method and refresh cluster state', function(){
+        this.scope.client.deleteIndex=function(index, success, failed) { return success(); };
+        this.scope.refreshClusterState=function(){};
+        spyOn(this.scope, "refreshClusterState").andReturn(true);
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "deleteIndex").andCallThrough();
+        spyOn(this.AlertService, "success").andReturn(true);
+        this.scope.deleteIndex("index_id");
+        expect(this.scope.client.deleteIndex).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.refreshClusterState).toHaveBeenCalled();
+    });
+
+    it('deleteIndex must display message if operation fails', function(){
+        this.scope.client.deleteIndex=function(index, success, failed) { return failed(); };
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "deleteIndex").andCallThrough();
+        spyOn(this.AlertService, "error").andReturn(true);
+        this.scope.deleteIndex("index_id");
+        expect(this.scope.client.deleteIndex).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.AlertService.error).toHaveBeenCalled();
+        expect(this.scope.updateModel).toHaveBeenCalled();
+    });
+
+    // clear cache
+    it('clearCache must invoke client method, display alert and refresh model', function(){
+        this.scope.client.clearCache=function(index_id, success, failed) { return success(); };
+        this.scope.refreshClusterState=function(){};
+        spyOn(this.scope, "refreshClusterState").andReturn(true);
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "clearCache").andCallThrough();
+        spyOn(this.AlertService, "success").andReturn(true);
+        this.scope.clearCache("index_id");
+        expect(this.scope.client.clearCache).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.refreshClusterState).toHaveBeenCalled();
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.success).toHaveBeenCalled();
+    });
+
+    it('clearCache must display message if operation fails', function(){
+        this.scope.client.clearCache=function(index_id, success, failed) { return failed(); };
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "clearCache").andCallThrough();
+        spyOn(this.AlertService, "error").andReturn(true);
+        this.scope.clearCache("index_id");
+        expect(this.scope.client.clearCache).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.error).toHaveBeenCalled();
+    });
+
+    // refresh index
+    it('refreshIndex must invoke client method, display alert and refresh model', function(){
+        this.scope.client.refreshIndex=function(index_id, success, failed) { return success(); };
+        this.scope.refreshClusterState=function(){};
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "refreshIndex").andCallThrough();
+        spyOn(this.AlertService, "success").andReturn(true);
+        this.scope.refreshIndex("index_id");
+        expect(this.scope.client.refreshIndex).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.success).toHaveBeenCalled();
+    });
+
+    it('refreshIndex must display message if operation fails', function(){
+        this.scope.client.refreshIndex=function(index_id, success, failed) { return failed(); };
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "refreshIndex").andCallThrough();
+        spyOn(this.AlertService, "error").andReturn(true);
+        this.scope.refreshIndex("index_id");
+        expect(this.scope.client.refreshIndex).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.error).toHaveBeenCalled();
+    });
+
+    // enable allocation
+    it('enableAllocation must invoke client method, display alert and refresh model', function(){
+        this.scope.client.enableShardAllocation=function(success, failed) { return success(); };
+        this.scope.refreshClusterState=function(){};
+        spyOn(this.scope, "refreshClusterState").andReturn(true);
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "enableShardAllocation").andCallThrough();
+        spyOn(this.AlertService, "success").andReturn(true);
+        this.scope.enableAllocation("node_id");
+        expect(this.scope.client.enableShardAllocation).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.refreshClusterState).toHaveBeenCalled();
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.success).toHaveBeenCalled();
+    });
+
+    it('enableAllocation must display an error message if operations fails', function(){
+        this.scope.client.enableShardAllocation=function(success, failed) { return failed(); };
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "enableShardAllocation").andCallThrough();
+        spyOn(this.AlertService, "error").andReturn(true);
+        this.scope.enableAllocation("node_id");
+        expect(this.scope.client.enableShardAllocation).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.error).toHaveBeenCalled();
+    });
+
+    // disable allocation
+    it('disableAllocation must invoke client method, display alert and refresh model', function(){
+        this.scope.client.disableShardAllocation=function(success, failed) { return success(); };
+        this.scope.refreshClusterState=function(){};
+        spyOn(this.scope, "refreshClusterState").andReturn(true);
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "disableShardAllocation").andCallThrough();
+        spyOn(this.AlertService, "success").andReturn(true);
+        this.scope.disableAllocation("node_id");
+        expect(this.scope.client.disableShardAllocation).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.refreshClusterState).toHaveBeenCalled();
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.success).toHaveBeenCalled();
+    });
+
+    it('disableAllocation must display an error message if operations fails', function(){
+        this.scope.client.disableShardAllocation=function(success, failed) { return failed(); };
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "disableShardAllocation").andCallThrough();
+        spyOn(this.AlertService, "error").andReturn(true);
+        this.scope.disableAllocation("node_id");
+        expect(this.scope.client.disableShardAllocation).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.error).toHaveBeenCalled();
+    });
+
+    // close index
+    it('closeIndex must invoke client method, display alert and refresh model', function(){
+        this.scope.client.closeIndex=function(index_id, success, failed) { return success(); };
+        this.scope.refreshClusterState=function(){};
+        spyOn(this.scope, "refreshClusterState").andReturn(true);
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "closeIndex").andCallThrough();
+        spyOn(this.AlertService, "success").andReturn(true);
+        this.scope.closeIndex("index_id");
+        expect(this.scope.client.closeIndex).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.refreshClusterState).toHaveBeenCalled();
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.success).toHaveBeenCalled();
+    });
+
+    it('closeIndex must display error message if operation fails', function(){
+        this.scope.client.closeIndex=function(index_id, success, failed) { return failed(); };
+        spyOn(this.scope.client, "closeIndex").andCallThrough();
+        spyOn(this.AlertService, "error").andReturn(true);
+        spyOn(this.scope, "updateModel").andCallThrough();
+        this.scope.closeIndex("index_id");
+        expect(this.scope.client.closeIndex).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.error).toHaveBeenCalled();
+    });
+
+    // open index
+    it('openIndex must invoke client method, display alert and refresh model', function(){
+        this.scope.client.openIndex=function(index_id, success, failed) { return success(); };
+        this.scope.refreshClusterState=function(){};
+        spyOn(this.scope, "refreshClusterState").andReturn(true);
+        spyOn(this.scope, "updateModel").andCallThrough();
+        spyOn(this.scope.client, "openIndex").andCallThrough();
+        spyOn(this.AlertService, "success").andReturn(true);
+        this.scope.openIndex("index_id");
+        expect(this.scope.client.openIndex).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.refreshClusterState).toHaveBeenCalled();
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.success).toHaveBeenCalled();
+    });
+
+    it('openIndex must display error message if operation fails', function(){
+        this.scope.client.openIndex=function(index_id, success, failed) { return failed(); };
+        spyOn(this.scope.client, "openIndex").andCallThrough();
+        spyOn(this.AlertService, "error").andReturn(true);
+        spyOn(this.scope, "updateModel").andCallThrough();
+        this.scope.openIndex("index_id");
+        expect(this.scope.client.openIndex).toHaveBeenCalledWith("index_id", jasmine.any(Function), jasmine.any(Function));
+        expect(this.scope.updateModel).toHaveBeenCalled();
+        expect(this.AlertService.error).toHaveBeenCalled();
+    });
 
 });
