4a34b06634891861e0369533a9e384794ecf9471
Leonardo Menezes <leonardo.menezes@xing.com>
2014-09-22 13:55:37 +0200

    version bump 1.3.3

--- dist/kopf.js
+++ dist/kopf.js
@@ -1981,7 +1981,7 @@ kopf.controller('CreateIndexController', ['$scope', '$location', '$timeout', 'Al
 	};
 }]);
 kopf.controller('GlobalController', ['$scope', '$location', '$timeout', '$http', '$q', '$sce', '$window', 'ConfirmDialogService', 'AlertService', 'SettingsService', 'ThemeService', 'ElasticService', function($scope, $location, $timeout, $http, $q, $sce, $window, ConfirmDialogService, AlertService, SettingsService, ThemeService, ElasticService) {
-	$scope.version = "1.3.2";
+	$scope.version = "1.3.3";
 	$scope.alert_service = AlertService;
     $scope.modal = new ModalControls();
 
