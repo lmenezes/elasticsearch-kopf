41e094c3a86f479fdaf9f02caaf0788929074496
Leonardo Menezes <leonardo.menezes@xing.com>
2014-10-14 21:32:53 +0200

    adapts elastic service to new style check

--- src/kopf/services/elastic.js
+++ src/kopf/services/elastic.js
@@ -1,31 +1,36 @@
-kopf.factory('ElasticService', ['$http', '$q', 'ExternalSettingsService', function($http, $q, ExternalSettingsService) {
-  this.client = null;
-  this.connection = null;
+kopf.factory('ElasticService', ['$http', '$q', 'ExternalSettingsService',
+  function($http, $q, ExternalSettingsService) {
 
-  this.connect = function(url) {
-    var root = ExternalSettingsService.getElasticsearchRootPath();
-    var with_credentials = ExternalSettingsService.withCredentials();
-    try {
-      this.client = null;
-      this.connection = null;
-      if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
-        url = 'http://' + url;
+    this.client = null;
+    this.connection = null;
+
+    this.connect = function(url) {
+      var root = ExternalSettingsService.getElasticsearchRootPath();
+      var withCredentials = ExternalSettingsService.withCredentials();
+      try {
+        this.client = null;
+        this.connection = null;
+        if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+          url = 'http://' + url;
+        }
+        this.connection = new ESConnection(url + root, withCredentials);
+        this.client = new ElasticClient(this.connection, $http, $q);
+      } catch (error) {
+        throw {
+          message: 'Error while connecting to [' + url + root + ']',
+          body: error
+        };
       }
-      this.connection = new ESConnection(url + root, with_credentials);
-      this.client = new ElasticClient(this.connection, $http, $q);
-    } catch (error) {
-      throw { message: 'Error while connecting to [' + url + root + ']', body: error };
-    }
-  };
+    };
 
-  this.isConnected = function() {
-    return isDefined(this.client);
-  };
+    this.isConnected = function() {
+      return isDefined(this.client);
+    };
 
-  this.getHost = function() {
-    return isDefined(this.connection) ? this.connection.host : '';
-  };
+    this.getHost = function() {
+      return isDefined(this.connection) ? this.connection.host : '';
+    };
 
-  return this;
+    return this;
 
-}]);
+  }]);
