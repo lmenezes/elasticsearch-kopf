e8ea8d146591745f05602243546bb2a1c5e8d0de
Leonardo Menezes <leonardo.menezes@xing.com>
2014-10-14 21:30:23 +0200

    adapts external settings service to style check

--- dist/kopf.js
+++ dist/kopf.js
@@ -3068,35 +3068,51 @@ kopf.factory('HostHistoryService', function() {
 
 kopf.factory('ExternalSettingsService', function($http, $q) {
 
+  var ES_ROOT_PATH = 'elasticsearch_root_path';
+
+  var WITH_CREDENTIALS = 'with_credentials';
+
   this.settings = null;
 
   this.getSettings = function() {
     if (!isDefined(this.settings)) {
       this.settings = {};
       var settings = this.settings;
-      var settings_future = $.ajax({ type: 'GET', url: './kopf_external_settings.json', dataType: 'json', async: false });
-      settings_future.done(function(data) {
+      var params = {
+        type: 'GET',
+        url: './kopf_external_settings.json',
+        dataType: 'json',
+        async: false
+      };
+      var settingsFuture = $.ajax(params);
+      settingsFuture.done(function(data) {
         try {
           Object.keys(data).forEach(function(setting) {
             settings[setting] = data[setting];
           });
         } catch (error) {
-          throw { message: 'Error processing external settings', body: data };
+          throw {
+            message: 'Error processing external settings',
+            body: data
+          };
         }
       });
-      settings_future.fail(function(error) {
-        throw { message: 'Error fetching external settings from file', body: error };
+      settingsFuture.fail(function(error) {
+        throw {
+          message: 'Error fetching external settings from file',
+          body: error
+        };
       });
     }
     return this.settings;
   };
 
   this.getElasticsearchRootPath = function() {
-    return this.getSettings().elasticsearch_root_path;
+    return this.getSettings()[ES_ROOT_PATH];
   };
 
   this.withCredentials = function() {
-    return this.getSettings().with_credentials;
+    return this.getSettings()[WITH_CREDENTIALS];
   };
 
   return this;
