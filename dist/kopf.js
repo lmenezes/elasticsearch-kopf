4ba02c306a4ef4ef89c1f1dea3992ba2b201edca
Leonardo Menezes <leonardo.menezes@xing.com>
2014-10-18 13:07:25 +0200

    adapts global controller to style check

--- dist/kopf.js
+++ dist/kopf.js
@@ -2153,151 +2153,164 @@ kopf.controller('CreateIndexController', ['$scope', 'AlertService', 'ElasticServ
   };
 }]);
 
-kopf.controller('GlobalController', ['$scope', '$location', '$timeout', '$http', '$q', '$sce', '$window', 'ConfirmDialogService', 'AlertService', 'SettingsService', 'ThemeService', 'ElasticService', function($scope, $location, $timeout, $http, $q, $sce, $window, ConfirmDialogService, AlertService, SettingsService, ThemeService, ElasticService) {
-  $scope.version = '1.3.6-SNAPSHOT';
-  $scope.alert_service = AlertService;
-  $scope.modal = new ModalControls();
+kopf.controller('GlobalController', ['$scope', '$location', '$timeout',
+  '$http', '$q', '$sce', '$window', 'ConfirmDialogService', 'AlertService',
+  'SettingsService', 'ThemeService', 'ElasticService',
+  function($scope, $location, $timeout, $http, $q, $sce, $window,
+           ConfirmDialogService, AlertService, SettingsService, ThemeService,
+           ElasticService) {
+    $scope.version = '1.3.6-SNAPSHOT';
+    $scope.alert_service = AlertService;
+    $scope.modal = new ModalControls();
 
-  $scope.home_screen = function() {
-    $('#cluster_option a').tab('show');
-  };
+    $scope.home_screen = function() {
+      $('#cluster_option a').tab('show');
+    };
 
-  $scope.getTheme = function() {
-    return ThemeService.getTheme();
-  };
+    $scope.getTheme = function() {
+      return ThemeService.getTheme();
+    };
 
-  $scope.broadcastMessage = function(message, args) {
-    $scope.$broadcast(message, args);
-  };
+    $scope.broadcastMessage = function(message, args) {
+      $scope.$broadcast(message, args);
+    };
 
-  $scope.readParameter = function(name) {
-    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec($window.location.href);
-    return isDefined(results) ? results[1] : null;
-  };
+    $scope.readParameter = function(name) {
+      var regExp = new RegExp('[\\?&]' + name + '=([^&#]*)');
+      var results = regExp.exec($window.location.href);
+      return isDefined(results) ? results[1] : null;
+    };
 
-  $scope.connect = function() {
-    try {
-      var host = 'http://localhost:9200'; // default
-      if ($location.host() !== '') { // not opening from fs
-        var location = $scope.readParameter('location');
-        if (isDefined(location)) {
-          host = location;
-        } else {
-          var url = $location.absUrl();
-          host = url.substring(0, url.indexOf('/_plugin/kopf'));
+    $scope.connect = function() {
+      try {
+        var host = 'http://localhost:9200'; // default
+        if ($location.host() !== '') { // not opening from fs
+          var location = $scope.readParameter('location');
+          if (isDefined(location)) {
+            host = location;
+          } else {
+            var url = $location.absUrl();
+            host = url.substring(0, url.indexOf('/_plugin/kopf'));
+          }
+        }
+        ElasticService.connect(host);
+        this.home_screen(); // FIXME: not even sure why this is here
+      } catch (error) {
+        AlertService.error(error.message, error.body);
+      }
+    };
+
+    $scope.connect();
+
+    $scope.alertClusterChanges = function() {
+      if (isDefined($scope.cluster)) {
+        var changes = $scope.cluster.changes;
+        if (changes.hasChanges()) {
+          if (changes.hasJoins()) {
+            var joins = changes.nodeJoins.map(function(node) {
+              return node.name + '[' + node.transport_address + ']';
+            });
+            AlertService.info(joins.length + ' new node(s) joined the cluster',
+                joins);
+          }
+          if (changes.hasLeaves()) {
+            var leaves = changes.nodeLeaves.map(function(node) {
+              return node.name + '[' + node.transport_address + ']';
+            });
+            AlertService.warn(changes.nodeLeaves.length +
+                    ' node(s) left the cluster', leaves);
+          }
+          if (changes.hasCreatedIndices()) {
+            var created = changes.indicesCreated.map(function(index) {
+              return index.name;
+            });
+            AlertService.info(changes.indicesCreated.length +
+                ' indices created: [' + created.join(',') + ']');
+          }
+          if (changes.hasDeletedIndices()) {
+            var deleted = changes.indicesDeleted.map(function(index) {
+              return index.name;
+            });
+            AlertService.info(changes.indicesDeleted.length +
+                ' indices deleted: [' + deleted.join(',') + ']');
+          }
         }
       }
-      ElasticService.connect(host);
-      this.home_screen(); // FIXME: not even sure why this is here
-    } catch (error) {
-      AlertService.error(error.message, error.body);
-    }
-  };
+    };
 
-  $scope.connect();
+    $scope.refreshClusterState = function() {
+      if (ElasticService.isConnected()) {
+        $timeout(function() {
+          ElasticService.client.getClusterDetail(
+              function(cluster) {
+                cluster.computeChanges($scope.cluster);
+                $scope.cluster = cluster;
+                $scope.alertClusterChanges();
+              },
+              function(error) {
+                AlertService.error('Error while retrieving cluster information',
+                    error);
+                $scope.cluster = null;
+              }
+          );
 
-  $scope.alertClusterChanges = function() {
-    if (isDefined($scope.cluster)) {
-      var changes = $scope.cluster.changes;
-      if (changes.hasChanges()) {
-        if (changes.hasJoins()) {
-          var joins = changes.nodeJoins.map(function(node) {
-            return node.name + '[' + node.transport_address + ']';
-          });
-          AlertService.info(joins.length + ' new node(s) joined the cluster', joins);
-        }
-        if (changes.hasLeaves()) {
-          var leaves = changes.nodeLeaves.map(function(node) {
-            return node.name + '[' + node.transport_address + ']';
-          });
-          AlertService.warn(changes.nodeLeaves.length + ' node(s) left the cluster', leaves);
-        }
-        if (changes.hasCreatedIndices()) {
-          var created = changes.indicesCreated.map(function(index) {
-            return index.name;
-          });
-          AlertService.info(changes.indicesCreated.length + ' indices created: [' + created.join(',') + ']');
-        }
-        if (changes.hasDeletedIndices()) {
-          var deleted = changes.indicesDeleted.map(function(index) {
-            return index.name;
-          });
-          AlertService.info(changes.indicesDeleted.length + ' indices deleted: [' + deleted.join(',') + ']');
-        }
+          ElasticService.client.getClusterHealth(
+              function(cluster) {
+                $scope.cluster_health = cluster;
+              },
+              function(error) {
+                $scope.cluster_health = null;
+                AlertService.error('Error connecting to [' + $scope.host + ']',
+                    error);
+              }
+          );
+        }, 100);
+      } else {
+        $scope.cluster = null;
+        $scope.cluster_health = null;
       }
-    }
-  };
+    };
 
-  $scope.refreshClusterState = function() {
-    if (ElasticService.isConnected()) {
+    $scope.autoRefreshCluster = function() {
+      $scope.refreshClusterState();
       $timeout(function() {
-        ElasticService.client.getClusterDetail(
-          function(cluster) {
-            cluster.computeChanges($scope.cluster);
-            $scope.cluster = cluster;
-            $scope.alertClusterChanges();
-          },
-          function(error) {
-            AlertService.error('Error while retrieving cluster information', error);
-            $scope.cluster = null;
-          }
-        );
+        $scope.autoRefreshCluster();
+      }, SettingsService.getRefreshInterval());
+    };
 
-        ElasticService.client.getClusterHealth(
-          function(cluster) {
-            $scope.cluster_health = cluster;
-          },
-          function(error) {
-            $scope.cluster_health = null;
-            AlertService.error('Error connecting to [' + $scope.host + ']', error);
-          }
-        );
-      }, 100);
-    } else {
-      $scope.cluster = null;
-      $scope.cluster_health = null;
-    }
-  };
+    $scope.autoRefreshCluster();
 
-  $scope.autoRefreshCluster = function() {
-    $scope.refreshClusterState();
-    $timeout(function() {
-      $scope.autoRefreshCluster();
-    }, SettingsService.getRefreshInterval());
-  };
+    $scope.hasConnection = function() {
+      return isDefined($scope.cluster_health);
+    };
 
-  $scope.autoRefreshCluster();
+    $scope.isActive = function(tab) {
+      return $('#' + tab).hasClass('active');
+    };
 
-  $scope.hasConnection = function() {
-    return isDefined($scope.cluster_health);
-  };
+    $scope.displayInfo = function(title, info) {
+      $scope.modal.title = title;
+      $scope.modal.info = $sce.trustAsHtml(JSONTree.create(info));
+      $('#modal_info').modal({show: true, backdrop: true});
+    };
 
-  $scope.isActive = function(tab) {
-    return $('#' + tab).hasClass('active');
-  };
+    $scope.getCurrentTime = function() {
+      return getTimeString(new Date());
+    };
 
-  $scope.displayInfo = function(title, info) {
-    $scope.modal.title = title;
-    $scope.modal.info = $sce.trustAsHtml(JSONTree.create(info));
-    $('#modal_info').modal({show: true, backdrop: true});
-  };
+    $scope.selectTab = function(event) {
+      AlertService.clear();
+      if (isDefined(event)) {
+        $scope.broadcastMessage(event, {});
+      }
+    };
 
-  $scope.getCurrentTime = function() {
-    return getTimeString(new Date());
-  };
+    $scope.updateModel = function(action) {
+      $scope.$apply(action);
+    };
 
-  $scope.selectTab = function(event) {
-    AlertService.clear();
-    if (isDefined(event)) {
-      $scope.broadcastMessage(event, {});
-    }
-  };
-
-  $scope.updateModel = function(action) {
-    $scope.$apply(action);
-  };
-
-}]);
+  }
+]);
 
 kopf.controller('IndexSettingsController', ['$scope', '$location', '$timeout',
   'IndexSettingsService', 'AlertService', 'ElasticService',
