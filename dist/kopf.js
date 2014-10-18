7f095f623afb70a1012e0b77663e796ed20fc23e
Leonardo Menezes <leonardo.menezes@xing.com>
2014-10-18 12:47:00 +0200

    adapts aliases model to style check

--- dist/kopf.js
+++ dist/kopf.js
@@ -14,21 +14,21 @@ function IndexAliases(index, aliases) {
 IndexAliases.diff = function(original, modified) {
   var differences = [];
   modified.forEach(function(ia) {
-    var is_new = true;
-    original.forEach(function(orig_ia) {
-      if (ia.index == orig_ia.index) {
-        is_new = false;
+    var isNew = true;
+    original.forEach(function(origIA) {
+      if (ia.index == origIA.index) {
+        isNew = false;
         ia.aliases.forEach(function(alias) {
-          var original_aliases = orig_ia.aliases.filter(function(original_alias) {
-            return alias.equals(original_alias);
+          var originalAliases = origIA.aliases.filter(function(originalAlias) {
+            return alias.equals(originalAlias);
           });
-          if (original_aliases.length === 0) {
+          if (originalAliases.length === 0) {
             differences.push(alias);
           }
         });
       }
     });
-    if (is_new) {
+    if (isNew) {
       ia.aliases.forEach(function(alias) {
         differences.push(alias);
       });
@@ -37,12 +37,12 @@ IndexAliases.diff = function(original, modified) {
   return differences;
 };
 
-function Alias(alias, index, filter, index_routing, search_routing) {
+function Alias(alias, index, filter, indexRouting, searchRouting) {
   this.alias = isDefined(alias) ? alias.toLowerCase() : '';
   this.index = isDefined(index) ? index.toLowerCase() : '';
   this.filter = isDefined(filter) ? filter : '';
-  this.index_routing = isDefined(index_routing) ? index_routing : '';
-  this.search_routing = isDefined(search_routing) ? search_routing : '';
+  this.index_routing = isDefined(indexRouting) ? indexRouting : '';
+  this.search_routing = isDefined(searchRouting) ? searchRouting : '';
 
   this.validate = function() {
     if (!notEmpty(this.alias)) {
@@ -53,13 +53,13 @@ function Alias(alias, index, filter, index_routing, search_routing) {
     }
   };
 
-  this.equals = function(other_alias) {
+  this.equals = function(otherAlias) {
     var equal =
-      (this.alias === other_alias.alias) &&
-      (this.index === other_alias.index) &&
-      (this.filter === other_alias.filter) &&
-      (this.index_routing === other_alias.index_routing) &&
-      (this.search_routing === other_alias.search_routing);
+        (this.alias === otherAlias.alias) &&
+        (this.index === otherAlias.index) &&
+        (this.filter === otherAlias.filter) &&
+        (this.index_routing === otherAlias.index_routing) &&
+        (this.search_routing === otherAlias.search_routing);
     return equal;
   };
 
@@ -85,7 +85,8 @@ function Alias(alias, index, filter, index_routing, search_routing) {
   };
 
   this.clone = function() {
-    return new Alias(this.alias, this.index, this.filter, this.index_routing, this.search_routing);
+    return new Alias(this.alias, this.index, this.filter, this.index_routing,
+        this.search_routing);
   };
 }
 
