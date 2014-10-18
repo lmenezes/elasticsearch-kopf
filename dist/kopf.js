2987a75ce584cb1694351a83ff8a4b541b1cc0ff
Leonardo Menezes <leonardo.menezes@xing.com>
2014-10-18 10:10:57 +0200

    adapts alias filter to style check

--- dist/kopf.js
+++ dist/kopf.js
@@ -3542,24 +3542,26 @@ function AliasFilter(index, alias) {
   };
 
   this.equals = function(other) {
-    return other !== null && this.index == other.index && this.alias == other.alias;
+    return (other !== null &&
+      this.index == other.index &&
+      this.alias == other.alias);
   };
 
   this.isBlank = function() {
     return !notEmpty(this.index) && !notEmpty(this.alias);
   };
 
-  this.matches = function(index_alias) {
+  this.matches = function(indexAlias) {
     if (this.isBlank()) {
       return true;
     } else {
       var matches = true;
       if (notEmpty(this.index)) {
-        matches = index_alias.index.indexOf(this.index) != -1;
+        matches = indexAlias.index.indexOf(this.index) != -1;
       }
       if (matches && notEmpty(this.alias)) {
         matches = false;
-        var aliases = index_alias.aliases;
+        var aliases = indexAlias.aliases;
         for (var i = 0; !matches && i < aliases.length; i++) {
           var alias = aliases[i];
           matches = alias.alias.indexOf(this.alias) != -1;
