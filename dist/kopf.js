9a57050121ee96c94a250f49405e35865eb0392a
Leonardo Menezes <leonardo.menezes@xing.com>
2014-10-18 13:37:39 +0200

    adapts cluster settings to style check

--- dist/kopf.js
+++ dist/kopf.js
@@ -2123,27 +2123,32 @@ kopf.controller('ClusterOverviewController', ['$scope', '$window',
   }
 ]);
 
-kopf.controller('ClusterSettingsController', ['$scope', '$location', '$timeout', 'AlertService', 'ElasticService', function($scope, $location, $timeout, AlertService, ElasticService) {
-  $scope.$on('loadClusterSettingsEvent', function() {
-    $('#cluster_settings_option a').tab('show');
-    $('#cluster_settings_tabs a:first').tab('show');
-    $('.setting-info').popover();
-    $scope.active_settings = 'transient'; // remember last active?
-    $scope.settings = new ClusterSettings($scope.cluster.settings);
-  });
+kopf.controller('ClusterSettingsController', ['$scope', '$location', '$timeout',
+  'AlertService', 'ElasticService',
+  function($scope, $location, $timeout, AlertService, ElasticService) {
+    $scope.$on('loadClusterSettingsEvent', function() {
+      $('#cluster_settings_option a').tab('show');
+      $('#cluster_settings_tabs a:first').tab('show');
+      $('.setting-info').popover();
+      $scope.active_settings = 'transient'; // remember last active?
+      $scope.settings = new ClusterSettings($scope.cluster.settings);
+    });
 
-  $scope.save = function() {
-    ElasticService.client.updateClusterSettings(JSON.stringify($scope.settings, undefined, ''),
-      function(response) {
-        AlertService.success('Cluster settings were successfully updated', response);
-        $scope.refreshClusterState();
-      },
-      function(error) {
-        AlertService.error('Error while updating cluster settings', error);
-      }
-    );
-  };
-}]);
+    $scope.save = function() {
+      var settings = JSON.stringify($scope.settings, undefined, '');
+      ElasticService.client.updateClusterSettings(settings,
+          function(response) {
+            AlertService.success('Cluster settings were successfully updated',
+                response);
+            $scope.refreshClusterState();
+          },
+          function(error) {
+            AlertService.error('Error while updating cluster settings', error);
+          }
+      );
+    };
+  }
+]);
 
 kopf.controller('CreateIndexController', ['$scope', 'AlertService',
   'ElasticService', 'AceEditorService',
