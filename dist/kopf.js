a4e3170d6e3b1d5712df93173f6b1a32f8ec9adc
Leonardo Menezes <leonardo.menezes@xing.com>
2014-10-18 13:35:05 +0200

    adapts analysis controller to style check

--- dist/kopf.js
+++ dist/kopf.js
@@ -1548,104 +1548,109 @@ kopf.controller('AliasesController', ['$scope', 'AlertService',
   }
 ]);
 
-kopf.controller('AnalysisController', ['$scope', '$location', '$timeout', 'AlertService', 'ElasticService', function($scope, $location, $timeout, AlertService, ElasticService) {
+kopf.controller('AnalysisController', ['$scope', '$location', '$timeout',
+  'AlertService', 'ElasticService',
+  function($scope, $location, $timeout, AlertService, ElasticService) {
 
-  $scope.indices = null;
+    $scope.indices = null;
 
-  // by index
-  $scope.field_index = null;
-  $scope.field_index_metadata = null;
-  $scope.field_type = '';
-  $scope.field_field = '';
-  $scope.field_text = '';
-  $scope.field_tokens = [];
-
-  // By analyzer
-  $scope.analyzer_index = '';
-  $scope.analyzer_index_metadata = null;
-  $scope.analyzer_analyzer = '';
-  $scope.analyzer_text = '';
-  $scope.analyzer_tokens = [];
-
-  $scope.$watch('field_index', function(current, previous) {
-    if (isDefined(current)) {
-      $scope.loadIndexTypes(current.name);
-    }
-  });
-
-  $scope.loadIndexTypes = function(index) {
+    // by index
+    $scope.field_index = null;
+    $scope.field_index_metadata = null;
     $scope.field_type = '';
     $scope.field_field = '';
-    if (notEmpty(index)) {
-      ElasticService.client.getIndexMetadata(index,
-        function(metadata) {
-          $scope.field_index_metadata = metadata;
-        },
-        function(error) {
-          $scope.field_index = '';
-          AlertService.error('Error while loading index metadata', error);
-        }
-      );
-    }
-  };
+    $scope.field_text = '';
+    $scope.field_tokens = [];
 
-  $scope.$watch('analyzer_index', function(current, previous) {
-    if (isDefined(current)) {
-      $scope.loadIndexAnalyzers(current.name);
-    }
-  });
-
-  $scope.loadIndexAnalyzers = function(index) {
+    // By analyzer
+    $scope.analyzer_index = '';
+    $scope.analyzer_index_metadata = null;
     $scope.analyzer_analyzer = '';
-    if (notEmpty(index)) {
-      ElasticService.client.getIndexMetadata(index,
-        function(metadata) {
-          $scope.analyzer_index_metadata = metadata;
-        },
-        function(error) {
-          $scope.analyzer_index = '';
-          AlertService.error('Error while loading index metadata', error);
-        }
-      );
-    }
-  };
+    $scope.analyzer_text = '';
+    $scope.analyzer_tokens = [];
 
+    $scope.$watch('field_index', function(current, previous) {
+      if (isDefined(current)) {
+        $scope.loadIndexTypes(current.name);
+      }
+    });
 
-  $scope.analyzeByField = function() {
-    if ($scope.field_field.length > 0 && $scope.field_text.length > 0) {
-      $scope.field_tokens = null;
-      ElasticService.client.analyzeByField($scope.field_index.name, $scope.field_type, $scope.field_field, $scope.field_text,
-        function(response) {
-          $scope.field_tokens = response;
-        },
-        function(error) {
-          $scope.field_tokens = null;
-          AlertService.error('Error while analyzing text', error);
-        }
-      );
-    }
-  };
+    $scope.loadIndexTypes = function(index) {
+      $scope.field_type = '';
+      $scope.field_field = '';
+      if (notEmpty(index)) {
+        ElasticService.client.getIndexMetadata(index,
+            function(metadata) {
+              $scope.field_index_metadata = metadata;
+            },
+            function(error) {
+              $scope.field_index = '';
+              AlertService.error('Error while loading index metadata', error);
+            }
+        );
+      }
+    };
 
-  $scope.analyzeByAnalyzer = function() {
-    if ($scope.analyzer_analyzer.length > 0 && $scope.analyzer_text.length > 0) {
-      $scope.analyzer_tokens = null;
-      ElasticService.client.analyzeByAnalyzer($scope.analyzer_index.name, $scope.analyzer_analyzer, $scope.analyzer_text,
-        function(response) {
-          $scope.analyzer_tokens = response;
-        },
-        function(error) {
-          $scope.analyzer_tokens = null;
-          AlertService.error('Error while analyzing text', error);
-        }
-      );
-    }
-  };
+    $scope.$watch('analyzer_index', function(current, previous) {
+      if (isDefined(current)) {
+        $scope.loadIndexAnalyzers(current.name);
+      }
+    });
 
-  $scope.$on('loadAnalysisEvent', function() {
-    $scope.indices = $scope.cluster.open_indices();
-  });
+    $scope.loadIndexAnalyzers = function(index) {
+      $scope.analyzer_analyzer = '';
+      if (notEmpty(index)) {
+        ElasticService.client.getIndexMetadata(index,
+            function(metadata) {
+              $scope.analyzer_index_metadata = metadata;
+            },
+            function(error) {
+              $scope.analyzer_index = '';
+              AlertService.error('Error while loading index metadata', error);
+            }
+        );
+      }
+    };
 
-}]);
+    $scope.analyzeByField = function() {
+      if ($scope.field_field.length > 0 && $scope.field_text.length > 0) {
+        $scope.field_tokens = null;
+        ElasticService.client.analyzeByField($scope.field_index.name,
+            $scope.field_type, $scope.field_field, $scope.field_text,
+            function(response) {
+              $scope.field_tokens = response;
+            },
+            function(error) {
+              $scope.field_tokens = null;
+              AlertService.error('Error while analyzing text', error);
+            }
+        );
+      }
+    };
+
+    $scope.analyzeByAnalyzer = function() {
+      if (notEmpty($scope.analyzer_analyzer) &&
+          notEmpty($scope.analyzer_text)) {
+        $scope.analyzer_tokens = null;
+        ElasticService.client.analyzeByAnalyzer($scope.analyzer_index.name,
+            $scope.analyzer_analyzer, $scope.analyzer_text,
+            function(response) {
+              $scope.analyzer_tokens = response;
+            },
+            function(error) {
+              $scope.analyzer_tokens = null;
+              AlertService.error('Error while analyzing text', error);
+            }
+        );
+      }
+    };
+
+    $scope.$on('loadAnalysisEvent', function() {
+      $scope.indices = $scope.cluster.open_indices();
+    });
+
+  }
+]);
 
 kopf.controller('ClusterHealthController', ['$scope', '$location', '$timeout',
   '$sce', 'AlertService', 'ConfirmDialogService', 'ElasticService',
