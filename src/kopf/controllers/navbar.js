8b021186fe8a0dd940f1683940f350d0a2314f90
Leonardo Menezes <leonardo.menezes@xing.com>
2014-10-18 13:39:30 +0200

    adapts navbar controller to style check

--- src/kopf/controllers/navbar.js
+++ src/kopf/controllers/navbar.js
@@ -1,41 +1,45 @@
-kopf.controller('NavbarController', ['$scope', 'SettingsService', 'ThemeService', 'ElasticService', 'AlertService', 'HostHistoryService', function($scope, SettingsService, ThemeService, ElasticService, AlertService, HostHistoryService) {
+kopf.controller('NavbarController', ['$scope', 'SettingsService',
+  'ThemeService', 'ElasticService', 'AlertService', 'HostHistoryService',
+  function($scope, SettingsService, ThemeService, ElasticService, AlertService,
+           HostHistoryService) {
 
-  $scope.new_refresh = SettingsService.getRefreshInterval();
-  $scope.theme = ThemeService.getTheme();
-  $scope.new_host = '';
-  $scope.current_host = ElasticService.getHost();
-  $scope.auto_adjust_layout = SettingsService.getAutoAdjustLayout();
-  $scope.host_history = HostHistoryService.getHostHistory();
+    $scope.new_refresh = SettingsService.getRefreshInterval();
+    $scope.theme = ThemeService.getTheme();
+    $scope.new_host = '';
+    $scope.current_host = ElasticService.getHost();
+    $scope.auto_adjust_layout = SettingsService.getAutoAdjustLayout();
+    $scope.host_history = HostHistoryService.getHostHistory();
 
-  $scope.handleConnectToHost = function(event) {
-    if (event.keyCode == 13 && notEmpty($scope.new_host)) {
-      $scope.connectToHost($scope.new_host);
-    }
-  };
+    $scope.handleConnectToHost = function(event) {
+      if (event.keyCode == 13 && notEmpty($scope.new_host)) {
+        $scope.connectToHost($scope.new_host);
+      }
+    };
 
-  $scope.connectToHost = function(host) {
-    try {
-      ElasticService.connect(host);
-      HostHistoryService.addToHistory(ElasticService.connection.host);
-      $scope.host_history = HostHistoryService.getHostHistory();
-    } catch (error) {
-      AlertService.error('Error while connecting to new target host', error);
-    } finally {
-      $scope.current_host = ElasticService.connection.host;
-      $scope.refreshClusterState();
-    }
-  };
+    $scope.connectToHost = function(host) {
+      try {
+        ElasticService.connect(host);
+        HostHistoryService.addToHistory(ElasticService.connection.host);
+        $scope.host_history = HostHistoryService.getHostHistory();
+      } catch (error) {
+        AlertService.error('Error while connecting to new target host', error);
+      } finally {
+        $scope.current_host = ElasticService.connection.host;
+        $scope.refreshClusterState();
+      }
+    };
 
-  $scope.changeRefresh = function() {
-    SettingsService.setRefreshInterval($scope.new_refresh);
-  };
+    $scope.changeRefresh = function() {
+      SettingsService.setRefreshInterval($scope.new_refresh);
+    };
 
-  $scope.changeTheme = function() {
-    ThemeService.setTheme($scope.theme);
-  };
+    $scope.changeTheme = function() {
+      ThemeService.setTheme($scope.theme);
+    };
 
-  $scope.setAutoAdjustLayout = function() {
-    SettingsService.setAutoAdjustLayout($scope.auto_adjust_layout);
-  };
+    $scope.setAutoAdjustLayout = function() {
+      SettingsService.setAutoAdjustLayout($scope.auto_adjust_layout);
+    };
 
-}]);
+  }
+]);
