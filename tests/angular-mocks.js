941799683e6433ab46a132c16d934d8d7b64daca
Leonardo Menezes <leonardo.menezes@xing.com>
2014-12-23 10:33:00 +0100

    upgrade to angularjs v1.3.8

--- tests/angular-mocks.js
+++ tests/angular-mocks.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.3
+ * @license AngularJS v1.3.8
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -1119,7 +1119,7 @@
    ```
    */
   angular.mock.$HttpBackendProvider = function() {
-    this.$get = ['$rootScope', createHttpBackendMock];
+    this.$get = ['$rootScope', '$timeout', createHttpBackendMock];
   };
 
   /**
@@ -1136,7 +1136,7 @@
    * @param {Object=} $browser Auto-flushing enabled if specified
    * @return {Object} Instance of $httpBackend mock
    */
-  function createHttpBackendMock($rootScope, $delegate, $browser) {
+  function createHttpBackendMock($rootScope, $timeout, $delegate, $browser) {
     var definitions = [],
         expectations = [],
         responses = [],
@@ -1149,7 +1149,7 @@
       return function() {
         return angular.isNumber(status)
             ? [status, data, headers, statusText]
-            : [200, status, data];
+            : [200, status, data, headers];
       };
     }
 
@@ -1166,7 +1166,9 @@
       }
 
       function wrapResponse(wrapped) {
-        if (!$browser && timeout && timeout.then) timeout.then(handleTimeout);
+        if (!$browser && timeout) {
+          timeout.then ? timeout.then(handleTimeout) : $timeout(handleTimeout, timeout);
+        }
 
         return handleResponse;
 
@@ -1281,7 +1283,7 @@
      * @param {string|RegExp|function(string)} url HTTP url or function that receives the url
      *   and returns true if the url match the current definition.
      * @param {(Object|function(Object))=} headers HTTP headers.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      * request is handled. You can save this object for later use and invoke `respond` again in
      * order to change how a matched request is handled.
      */
@@ -1295,7 +1297,7 @@
      * @param {string|RegExp|function(string)} url HTTP url or function that receives the url
      *   and returns true if the url match the current definition.
      * @param {(Object|function(Object))=} headers HTTP headers.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      * request is handled. You can save this object for later use and invoke `respond` again in
      * order to change how a matched request is handled.
      */
@@ -1309,7 +1311,7 @@
      * @param {string|RegExp|function(string)} url HTTP url or function that receives the url
      *   and returns true if the url match the current definition.
      * @param {(Object|function(Object))=} headers HTTP headers.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      * request is handled. You can save this object for later use and invoke `respond` again in
      * order to change how a matched request is handled.
      */
@@ -1325,7 +1327,7 @@
      * @param {(string|RegExp|function(string))=} data HTTP request body or function that receives
      *   data string and returns true if the data is as expected.
      * @param {(Object|function(Object))=} headers HTTP headers.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      * request is handled. You can save this object for later use and invoke `respond` again in
      * order to change how a matched request is handled.
      */
@@ -1341,7 +1343,7 @@
      * @param {(string|RegExp|function(string))=} data HTTP request body or function that receives
      *   data string and returns true if the data is as expected.
      * @param {(Object|function(Object))=} headers HTTP headers.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      * request is handled. You can save this object for later use and invoke `respond` again in
      * order to change how a matched request is handled.
      */
@@ -1354,7 +1356,7 @@
      *
      * @param {string|RegExp|function(string)} url HTTP url or function that receives the url
      *   and returns true if the url match the current definition.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      * request is handled. You can save this object for later use and invoke `respond` again in
      * order to change how a matched request is handled.
      */
@@ -1375,7 +1377,7 @@
      *  is in JSON format.
      * @param {(Object|function(Object))=} headers HTTP headers or function that receives http header
      *   object and returns true if the headers match the current expectation.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      *  request is handled. You can save this object for later use and invoke `respond` again in
      *  order to change how a matched request is handled.
      *
@@ -1410,7 +1412,7 @@
      * @param {string|RegExp|function(string)} url HTTP url or function that receives the url
      *   and returns true if the url match the current definition.
      * @param {Object=} headers HTTP headers.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      * request is handled. You can save this object for later use and invoke `respond` again in
      * order to change how a matched request is handled. See #expect for more info.
      */
@@ -1424,7 +1426,7 @@
      * @param {string|RegExp|function(string)} url HTTP url or function that receives the url
      *   and returns true if the url match the current definition.
      * @param {Object=} headers HTTP headers.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      *   request is handled. You can save this object for later use and invoke `respond` again in
      *   order to change how a matched request is handled.
      */
@@ -1438,7 +1440,7 @@
      * @param {string|RegExp|function(string)} url HTTP url or function that receives the url
      *   and returns true if the url match the current definition.
      * @param {Object=} headers HTTP headers.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      *   request is handled. You can save this object for later use and invoke `respond` again in
      *   order to change how a matched request is handled.
      */
@@ -1455,7 +1457,7 @@
      *  receives data string and returns true if the data is as expected, or Object if request body
      *  is in JSON format.
      * @param {Object=} headers HTTP headers.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      *   request is handled. You can save this object for later use and invoke `respond` again in
      *   order to change how a matched request is handled.
      */
@@ -1472,7 +1474,7 @@
      *  receives data string and returns true if the data is as expected, or Object if request body
      *  is in JSON format.
      * @param {Object=} headers HTTP headers.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      *   request is handled. You can save this object for later use and invoke `respond` again in
      *   order to change how a matched request is handled.
      */
@@ -1489,7 +1491,7 @@
      *  receives data string and returns true if the data is as expected, or Object if request body
      *  is in JSON format.
      * @param {Object=} headers HTTP headers.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      *   request is handled. You can save this object for later use and invoke `respond` again in
      *   order to change how a matched request is handled.
      */
@@ -1502,7 +1504,7 @@
      *
      * @param {string|RegExp|function(string)} url HTTP url or function that receives the url
      *   and returns true if the url match the current definition.
-     * @returns {requestHandler} Returns an object with `respond` method that control how a matched
+     * @returns {requestHandler} Returns an object with `respond` method that controls how a matched
      *   request is handled. You can save this object for later use and invoke `respond` again in
      *   order to change how a matched request is handled.
      */
@@ -2040,7 +2042,7 @@
    */
   angular.mock.e2e = {};
   angular.mock.e2e.$httpBackendDecorator =
-      ['$rootScope', '$delegate', '$browser', createHttpBackendMock];
+      ['$rootScope', '$timeout', '$delegate', '$browser', createHttpBackendMock];
 
 
   /**
@@ -2054,7 +2056,7 @@
    *
    * In addition to all the regular `Scope` methods, the following helper methods are available:
    */
-  angular.mock.$RootScopeDecorator = function($delegate) {
+  angular.mock.$RootScopeDecorator = ['$delegate', function($delegate) {
 
     var $rootScopePrototype = Object.getPrototypeOf($delegate);
 
@@ -2126,7 +2128,7 @@
 
       return count;
     }
-  };
+  }];
 
 
   if (window.jasmine || window.mocha) {
