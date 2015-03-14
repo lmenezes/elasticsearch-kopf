e01a54e4dd3b8812d23a455b2de2a14185f4b479
Leonardo Menezes <leonardo.menezes@xing.com>
2015-03-14 20:10:52 +0100

    save in localStorage values for refresh rate and theme
    values on the local storage will override the values set on the external settings file
    
    closes #243

--- src/kopf/services/external_settings.js
+++ src/kopf/services/external_settings.js
@@ -1,19 +1,31 @@
-kopf.factory('ExternalSettingsService', function($http, $q) {
+kopf.factory('ExternalSettingsService', ['DebugService',
+  function(DebugService) {
 
-  var ES_ROOT_PATH = 'elasticsearch_root_path';
+    var KEY = 'kopfSettings';
 
-  var WITH_CREDENTIALS = 'with_credentials';
+    var ES_ROOT_PATH = 'elasticsearch_root_path';
 
-  var REFRESH_RATE = 'refresh_rate';
+    var WITH_CREDENTIALS = 'with_credentials';
 
-  var THEME = 'theme';
+    var REFRESH_RATE = 'refresh_rate';
 
-  this.settings = null;
+    var THEME = 'theme';
 
-  this.getSettings = function() {
-    if (!isDefined(this.settings)) {
-      this.settings = {};
-      var settings = this.settings;
+    var UPDATABLE_SETTINGS = [REFRESH_RATE, THEME];
+
+    this.settings = null;
+
+    this.getSettings = function() {
+      if (!isDefined(this.settings)) {
+        this.settings = this.fetchSettings();
+        var localSettings = this.loadLocalSettings();
+        this.updateSettings(localSettings);
+      }
+      return this.settings;
+    };
+
+    this.fetchSettings = function() {
+      var settings = {};
       var params = {
         type: 'GET',
         url: './kopf_external_settings.json',
@@ -39,34 +51,68 @@ kopf.factory('ExternalSettingsService', function($http, $q) {
           body: error
         };
       });
-    }
-    return this.settings;
-  };
+      return settings;
+    };
 
-  this.getElasticsearchRootPath = function() {
-    return this.getSettings()[ES_ROOT_PATH];
-  };
+    this.getElasticsearchRootPath = function() {
+      return this.getSettings()[ES_ROOT_PATH];
+    };
 
-  this.withCredentials = function() {
-    return this.getSettings()[WITH_CREDENTIALS];
-  };
+    this.withCredentials = function() {
+      return this.getSettings()[WITH_CREDENTIALS];
+    };
 
-  this.getRefreshRate = function() {
-    return this.getSettings()[REFRESH_RATE];
-  };
+    this.getRefreshRate = function() {
+      return this.getSettings()[REFRESH_RATE];
+    };
 
-  this.setRefreshRate = function(rate) {
-    this.getSettings()[REFRESH_RATE] = rate;
-  };
+    this.setRefreshRate = function(rate) {
+      this.getSettings()[REFRESH_RATE] = rate;
+      this.saveSettings();
+    };
 
-  this.getTheme = function() {
-    return this.getSettings()[THEME];
-  };
+    this.getTheme = function() {
+      return this.getSettings()[THEME];
+    };
 
-  this.setTheme = function(theme) {
-    this.getSettings()[THEME] = theme;
-  };
+    this.setTheme = function(theme) {
+      this.getSettings()[THEME] = theme;
+      this.saveSettings();
+    };
 
-  return this;
+    this.saveSettings = function() {
+      var settings = {};
+      for (var setting in this.settings) {
+        if (UPDATABLE_SETTINGS.indexOf(setting) >= 0) {
+          settings[setting] = this.settings[setting];
+        }
+      }
+      localStorage.setItem(KEY, JSON.stringify(settings));
+    };
 
-});
+    this.loadLocalSettings = function() {
+      var settings = {};
+      try {
+        var content = localStorage.getItem(KEY);
+        if (content) {
+          settings = JSON.parse(content);
+        }
+      } catch (error) {
+        DebugService.debug('Error while loading settings from local storage');
+      }
+      return settings;
+    };
+
+    this.updateSettings = function(settings) {
+      if (settings) {
+        for (var setting in settings) {
+          if (UPDATABLE_SETTINGS.indexOf(setting) >= 0) {
+            this.settings[setting] = settings[setting];
+          }
+        }
+      }
+    };
+
+    return this;
+
+  }]);
