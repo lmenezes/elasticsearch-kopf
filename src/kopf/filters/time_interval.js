5a3c16e2a485edd98c1069bfcce8d65ce719d1c5
Leonardo Menezes <leonardo.menezes@xing.com>
2015-03-20 19:41:42 +0100

    use shorter format for nodes uptime

--- src/kopf/filters/time_interval.js
+++ src/kopf/filters/time_interval.js
@@ -1,29 +1,24 @@
 kopf.filter('timeInterval', function() {
 
-  var UNITS = ['year', 'month', 'day', 'hour', 'minute'];
+  var UNITS = ['yr', 'mo', 'd', 'h', 'min'];
 
   var UNIT_MEASURE = {
-    year: 31536000000,
-    month: 2678400000,
-    week: 604800000,
-    day: 86400000,
-    hour: 3600000,
-    minute: 60000
+    yr: 31536000000,
+    mo: 2678400000,
+    wk: 604800000,
+    d: 86400000,
+    h: 3600000,
+    min: 60000
   };
 
   function stringify(seconds) {
 
     var result = 'less than a minute';
 
-    function format(number, unit) {
-      var caption = ' ' + unit + ((number > 1) ? 's' : '');
-      return number + caption;
-    }
-
     for (var idx = 0; idx < UNITS.length; idx++) {
       var amount = Math.floor(seconds / UNIT_MEASURE[UNITS[idx]]);
       if (amount) {
-        result = format(amount, UNITS[idx]);
+        result = amount + UNITS[idx] + '.';
         break;
       }
     }
--- tests/jasmine/filters/time_interval.tests.js
+++ tests/jasmine/filters/time_interval.tests.js
@@ -6,13 +6,13 @@ describe('filter', function() {
 
     it('should convert boolean values to unicode checkmark or cross',
         inject(function(timeIntervalFilter) {
-          expect(timeIntervalFilter(100000)).toBe('1 minute');
-          expect(timeIntervalFilter(1000000)).toBe('16 minutes');
-          expect(timeIntervalFilter(10000000)).toBe('2 hours');
-          expect(timeIntervalFilter(100000000)).toBe('1 day');
-          expect(timeIntervalFilter(1000000000)).toBe('11 days');
-          expect(timeIntervalFilter(10000000000)).toBe('3 months');
-          expect(timeIntervalFilter(100000000000)).toBe('3 years');
+          expect(timeIntervalFilter(100000)).toBe('1min.');
+          expect(timeIntervalFilter(1000000)).toBe('16min.');
+          expect(timeIntervalFilter(10000000)).toBe('2h.');
+          expect(timeIntervalFilter(100000000)).toBe('1d.');
+          expect(timeIntervalFilter(1000000000)).toBe('11d.');
+          expect(timeIntervalFilter(10000000000)).toBe('3mo.');
+          expect(timeIntervalFilter(100000000000)).toBe('3yr.');
         }));
   });
 });
