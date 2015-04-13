be58e548e953d7ff7bb8b3dd583300a20e9a1888
Leonardo Menezes <leonardo.menezes@xing.com>
2015-04-13 22:10:15 +0200

    version bump 1.4.8

--- dist/kopf.js
+++ dist/kopf.js
@@ -994,7 +994,7 @@ kopf.controller('GlobalController', ['$scope', '$location', '$sce', '$window',
   function($scope, $location, $sce, $window, AlertService, ElasticService,
            ExternalSettingsService, PageService) {
 
-    $scope.version = '1.4.7';
+    $scope.version = '1.4.8';
 
     $scope.modal = new ModalControls();
 
