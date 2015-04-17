28af437dae8d5962363392065adbdea6b66f3211
Leonardo Menezes <leonardo.menezes@xing.com>
2015-04-17 18:04:50 +0200

    removed active css indicator from navbar

--- dist/kopf.js
+++ dist/kopf.js
@@ -1869,34 +1869,12 @@ kopf.controller('WarmersController', [
 kopf.directive('ngNavbarSection', ['$location', 'ElasticService',
   function($location, ElasticService) {
 
-    function link(scope, elem, attrs) {
-      scope.$watch(
-          function() {
-            return $location.path();
-          },
-          function() {
-            var name = attrs.name;
-            var active = name === $location.path().substring(1);
-            if (active) {
-              elem.addClass('active');
-            } else {
-              elem.removeClass('active');
-            }
-          }
-      );
-    }
-
     return {
-      link: link,
       template: function(elem, attrs) {
         var visible = ElasticService.versionCheck(attrs.version);
         if (visible) {
           var name = attrs.name;
           var icon = attrs.icon;
-          var active = name === $location.path().substring(1);
-          if (active) {
-            elem.addClass('active');
-          }
           return '<a href="#!' + name + '">' +
               '<i class="fa fa-fw ' + icon + '"></i> ' + name +
               '</a>';
--- tests/jasmine/directives/navbar_section.tests.js
+++ tests/jasmine/directives/navbar_section.tests.js
@@ -16,48 +16,4 @@ describe('ngNavbarSection', function() {
     expect(element.html()).toContain('<a href="#!snapshot"><i class="fa fa-fw fa-camera"></i> snapshot</a>');
   });
 
-  it('Creates correct HTML for active element', function() {
-    spyOn($location, 'path').andReturn('/snapshot');
-    var element = $compile('<li ng-navbar-section name="snapshot" icon="fa-camera"></li>')($rootScope);
-    $rootScope.$digest();
-    expect($location.path).toHaveBeenCalled();
-    expect(element.attr('class')).toEqual('active ng-scope');
-  });
-
-  it('Creates correct HTML for non active element', function() {
-    spyOn($location, 'path').andReturn('/cluster');
-    var element = $compile('<li ng-navbar-section name="snapshot" icon="fa-camera"></li>')($rootScope);
-    $rootScope.$digest();
-    expect($location.path).toHaveBeenCalled();
-    expect(element.attr('class')).toEqual('ng-scope');
-  });
-
-  it('removes active class when location has changed', function() {
-    $location.path = function() { return '/snapshot'; };
-    spyOn($location, 'path').andCallThrough();
-    var element = $compile('<li ng-navbar-section name="snapshot" icon="fa-camera"></li>')($rootScope);
-    $rootScope.$digest();
-    expect($location.path).toHaveBeenCalled();
-    expect(element.attr('class')).toEqual('active ng-scope');
-    $location.path = function() { return '/cluster'; };
-    spyOn($location, 'path').andCallThrough();
-    $rootScope.$digest();
-    expect($location.path).toHaveBeenCalled();
-    expect(element.attr('class')).toEqual('ng-scope');
-  });
-
-  it('adds active class when location has changed', function() {
-    $location.path = function() { return '/cluster'; };
-    spyOn($location, 'path').andCallThrough();
-    var element = $compile('<li ng-navbar-section name="snapshot" icon="fa-camera"></li>')($rootScope);
-    $rootScope.$digest();
-    expect($location.path).toHaveBeenCalled();
-    expect(element.attr('class')).toEqual('ng-scope');
-    $location.path = function() { return '/snapshot'; };
-    spyOn($location, 'path').andCallThrough();
-    $rootScope.$digest();
-    expect($location.path).toHaveBeenCalled();
-    expect(element.attr('class')).toEqual('ng-scope active');
-  });
-
 });
