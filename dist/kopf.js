f83871be735c735b9c0f955d1cff8b21611b78da
Leonardo Menezes <leonardo.menezes@xing.com>
2015-05-09 21:38:22 +0200

    handle exception when changing favicon
    closes #272

--- dist/kopf.js
+++ dist/kopf.js
@@ -5109,9 +5109,13 @@ kopf.factory('HostHistoryService', function() {
 
 });
 
-kopf.factory('PageService', ['ElasticService', '$rootScope', '$document',
-  function(ElasticService, $rootScope, $document) {
+kopf.factory('PageService', ['ElasticService', 'DebugService', '$rootScope',
+  '$document', function(ElasticService, DebugService, $rootScope, $document) {
+
     var instance = this;
+
+    var clusterStatus;
+    var clusterName;
 
     var link = $document[0].querySelector('link[rel~=\'icon\']');
 
@@ -5131,29 +5135,35 @@ kopf.factory('PageService', ['ElasticService', '$rootScope', '$document',
         }
     );
 
-    this.setPageTitle = function(clusterName) {
-      if (clusterName) {
-        $rootScope.title = 'kopf[' + clusterName + ']';
+    this.setPageTitle = function(name) {
+      if (name && name !== clusterName) {
+        clusterName = name;
+        $rootScope.title = 'kopf[' + name + ']';
       } else {
         $rootScope.title = 'kopf - no connection';
       }
     };
 
     this.setFavIconColor = function(status) {
-      if (link) {
-        var colors = {green: '#468847', yellow: '#c09853', red: '#B94A48'};
-        var color = status ? colors[status] : '#333';
-        var canvas = $document[0].createElement('canvas');
-        canvas.width = 32;
-        canvas.height = 32;
-        var context = canvas.getContext('2d');
-        context.drawImage(img, 0, 0);
-        context.globalCompositeOperation = 'source-in';
-        context.fillStyle = color;
-        context.fillRect(0, 0, 32, 32);
-        context.fill();
-        link.type = 'image/x-icon';
-        link.href = canvas.toDataURL();
+      if (link && clusterStatus !== status) {
+        try {
+          clusterStatus = status;
+          var colors = {green: '#468847', yellow: '#c09853', red: '#B94A48'};
+          var color = status ? colors[status] : '#333';
+          var canvas = $document[0].createElement('canvas');
+          canvas.width = 32;
+          canvas.height = 32;
+          var context = canvas.getContext('2d');
+          context.drawImage(img, 0, 0);
+          context.globalCompositeOperation = 'source-in';
+          context.fillStyle = color;
+          context.fillRect(0, 0, 32, 32);
+          context.fill();
+          link.type = 'image/x-icon';
+          link.href = canvas.toDataURL();
+        } catch (exception) {
+          DebugService.debug('Error while changing favicon', exception);
+        }
       }
     };
 
