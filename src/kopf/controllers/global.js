8b8248a8e35bf153edb4dba84106bb974a75e55c
Leonardo Menezes <leonardo.menezes@xing.com>
2015-05-14 10:37:54 +0200

    release 1.5.3

--- src/kopf/controllers/global.js
+++ src/kopf/controllers/global.js
@@ -3,7 +3,7 @@ kopf.controller('GlobalController', ['$scope', '$location', '$sce', '$window',
   function($scope, $location, $sce, $window, AlertService, ElasticService,
            ExternalSettingsService, PageService) {
 
-    $scope.version = '1.5.3-SNAPSHOT';
+    $scope.version = '1.5.3';
 
     $scope.modal = new ModalControls();
 
