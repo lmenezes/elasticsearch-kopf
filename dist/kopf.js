fc9e116c18abef8f736eef53353cd0fde797766f
Leonardo Menezes <leonardo.menezes@xing.com>
2015-10-30 17:49:14 +0100

    moved to 2.0.1-snapshot

--- dist/kopf.js
+++ dist/kopf.js
@@ -1221,7 +1221,7 @@ kopf.controller('GlobalController', ['$scope', '$location', '$sce', '$window',
   function($scope, $location, $sce, $window, AlertService, ElasticService,
            ExternalSettingsService, PageService) {
 
-    $scope.version = '2.0.0';
+    $scope.version = '2.0.1-SNAPSHOT';
 
     $scope.modal = new ModalControls();
 
