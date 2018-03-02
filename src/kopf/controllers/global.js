addda3d517c88441bf046c9a36062f65ba6c5a16
Michael Wyraz <michael.wyraz@evermind.de>
2018-03-02 11:54:45 +0100

    Decouple version check from own version

--- src/kopf/controllers/global.js
+++ src/kopf/controllers/global.js
@@ -15,7 +15,7 @@ kopf.controller('GlobalController', ['$scope', '$location', '$sce', '$window',
           var version = ElasticService.getVersion();
           if (version && version.isValid()) {
             var major = version.getMajor();
-            if (major != parseInt($scope.version.charAt(0))) {
+            if (major < 5 || major > 6) {
               AlertService.warn(
                   'This version of kopf is not compatible with your ES version',
                   'Upgrading to newest supported version is recommended'
